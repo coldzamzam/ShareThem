30ed5f0c6f8347ae3c41bb65843bd09f9cdb71c4
coldzamzam <zamaqsa99@gmail.com>
2025-06-03 15:45:41 +0700

    splash screen abal abal

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_shareit/firebase_options.dart';
 import 'package:flutter_shareit/scaffolds/home_page.dart';
 import 'package:flutter_shareit/scaffolds/login_page.dart';
-import 'package:firebase_core/firebase_core.dart'; 
-import 'package:flutter_shareit/firebase_options.dart'; 
+import 'package:flutter_shareit/screens/splash_screen.dart';
 
-void main() async{
-// Ensure Flutter widgets are initialized
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Firebase
@@ -13,45 +13,27 @@ void main() async{
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // Now you can run your app
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter ShareIt',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
+        useMaterial3: true,
+      ),
       initialRoute: '/',
       routes: {
-        '/': (_) => HomePage(),
-        '/login': (_) => LoginPage(),
-        '/home': (_) => HomePage()
-
+        '/': (_) => const SplashScreen(),
+        '/login': (_) => const LoginPage(),
+        '/home': (_) => const HomePage(),
       },
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
-        useMaterial3: true
-      ),
-      debugShowCheckedModeBanner: false
     );
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Delay splash screen for 3 seconds, then navigate
+    Future.delayed(const Duration(seconds: 4), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF554DDE), // Ungu gelap
+              Color(0xFF8E44AD), // Ungu muda
+            ],
+          ),
+        ),
+        child: Center(
+          child: Lottie.asset(
+            'assets/animations/splash_screen.json',
+            width: 200,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    );
+  }
+}
